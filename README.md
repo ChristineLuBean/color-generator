142f59b8e737973ae16936356a7da00183ca737a
Christine LuBean <christinemlubean@gmail.com>
2023-09-26 15:11:54 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# color-generator
+# Cocktail Recipes Web Application
+A website offering numerous cocktail recipes from the CocktailDB API, simplifying the discovery and preparation of various beverages.
+
+**Link to project:**
+
+![screenshot of cocktail recipes web application](http://placecorgi.com/1200/650)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript
+
+## Lessons Learned:
---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+// Fetches data from the specified URL and performs actions based on the response.
+function getFetch() {
+
+  const url = "http://colormind.io/api/";
+  const data = {
+    model : "default",
+  }
+
+  const http = new XMLHttpRequest();
+
+  http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+      const palette = JSON.parse(http.responseText).result;
+      console.log(palette)
+      for (let i = 0; i < palette.length; i++) {
+        console.log(`rgb(${palette[i]})`)
+        console.log('color-rgb-' + i)
+        document.getElementById('color-rgb-'+ i).innerText = `rgb(${palette[i]})`
+        document.documentElement.style.setProperty(`--palette-color-${i}`, `rgb(${palette[i]})`);
+        // document.documentElement.style.cssText = `--palette-color-${i}: rgb(${palette[i]}`;
+      }
+    }
+  }
+
+  http.open("POST", url, true);
+  http.send(JSON.stringify(data));
+
+}
+
+// Event listener for button click
+document.querySelector('button').addEventListener('click', getFetch);
+
+//Fetches data from thecocktaildb API and populates the HTML elements with cocktail names and images.
+window.onload = function() {
+  getFetch()
+}
